e55638e0a25f4eccb4d3fdc09a320ca70789e09f
shr-23-uti <vshrutivishwakarmas@gmail.com>
2025-02-21 15:32:19 +0530

    [Shruti] Add. Print Hello From Bridgelabz using Rest Call

---
+++ src/main/java/com/springintro/helloworld/WebController.java
@@ -0,0 +1,15 @@
+package com.springintro.helloworld;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class WebController {
+
+    @GetMapping("/hello")
+    public String print() {
+        return "Hello from BridgeLabz";
+    }
+}
