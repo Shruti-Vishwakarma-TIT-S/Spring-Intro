c5969002d99e03448ea6cdf5bb3732423ebd42c6
shr-23-uti <vshrutivishwakarmas@gmail.com>
2025-02-21 14:59:02 +0530

    [Shruti] Add. Print Hello From Bridgelabz using MVC controller

--- src/main/java/com/springintro/helloworld/Controller.java
+++ src/main/java/com/springintro/helloworld/Controller.java
@@ -1,13 +1,14 @@
 package com.springintro.helloworld;
 
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@org.springframework.stereotype.Controller
 public class Controller {
 
-    @GetMapping("/hello")
-    public String print() {
-        return "Hello from BridgeLabz";
+    @GetMapping("/web")
+    public String print(Model model) {
+        model.addAttribute("message", "Hello from BridgeLabz");
+        return "hello";
     }
 }
